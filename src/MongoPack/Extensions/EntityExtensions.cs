c06beb1128c8f4dfd2ac921f130dd8bee528cea4
ktomkow <krzysztof.tomkow@gmail.com>
2021-10-11 22:35:05 +0200

    Let persistency package to set non public id field

---
+++ src/MongoPack/Extensions/EntityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using ProjectsCore.Models;
+using System;
+
+namespace MongoPack.Extensions
+{
+    internal static class EntityExtensions
+    {
+        internal static void SetId<TKey, TEntity>(this TEntity entity, TKey key ) where TKey : struct where TEntity : Entity<TKey>
+        {
+            Type entityType = entity.GetType();
+            var setter = entityType.GetField("id", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+            setter.SetValue(entity, key);
+        }
+    }
+}
---
+++ tests/MongoPack.UnitTests/SettingNonPublicIdTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using MongoPack.Extensions;
+using ProjectsCore.Models;
+using Xunit;
+
+namespace MongoPack.UnitTests
+{
+    public class SettingNonPublicIdTests
+    {
+        [Fact]
+        public void SetId_TypeInt()
+        {
+            int id = 15;
+            ClassWithoutPublicIdSetter instance = new ClassWithoutPublicIdSetter();
+
+            instance.SetId(id);
+
+            instance.Id.Should().Be(id);
+        }
+
+        private class ClassWithoutPublicIdSetter : Entity<int>
+        {
+
+        }
+    }
+}
